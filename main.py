928fc4475eed70ca13092d6f8b9408aff7bd554a
PheonixPhung <phung.huynhsaca@gmail.com>
2026-03-04 23:07:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print ("Welcome to the Possibilities of Moses")
 print ("The bush in front of your feet is burning with flames, yet it's still alive.")
+print ("A voice calls from the burning bush. 'Moses! Moses! Here am I. I am the God of thy father and all those before. I have heard the sorrow of my people. I am calling you to be their saviour.'")
+print ("What shall you do, Moses? Shall you 'flee' or 'follow'")
+answer = input("Enter your decision:")
+if answer == "follow":
+  print("You are unsure, yet you stay in the sight of the Lord. For He reassures you, He chose you for a reason, even with all your flaws.")
+elif answer == "flee":
+  print("You have run far into the desert. Your name is not heard, and your blessings are taken. Further shall you stray, and your bones and figs rot. Stray Ending")
+print ("If you have chosen to 'flee', your journey has ended! Please leave!")
+
+print ("Your journey to Egypt was difficult. You are faced with Pharaoh. You have stated, 'Let my people go! The God of Israel has demanded that you free his people!' Pharaoh denied.")
+
+        
