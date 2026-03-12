9320343c258995134379f16be796074f040dd474
Aigo05 <lilaigo210@gmail.com>
2026-03-12 10:11:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,99 +31,107 @@ while True:
     restart = input("(yes/no): ")
     if restart != "yes":
       break
+    else:
+      continue
 
-print ("Pharaoh has hardened his heart against God and the Israelites. He will not let God's people go, so be it. For God shall not let Pharaoh's people survive.")
-print ("The first of the plagues has begun. You have told Pharaoh that the God of the Hebrews shall punish him for his disobedience. Pharaoh laughs in your face, his eyes glint with the hardened heart of his.")
+  print ("Pharaoh has hardened his heart against God and the Israelites. He will not let God's people go, so be it. For God shall not let Pharaoh's people survive.")
+  print ("The first of the plagues has begun. You have told Pharaoh that the God of the Hebrews shall punish him for his disobedience. Pharaoh laughs in your face, his eyes glint with the hardened heart of his.")
 
-print ("The Nile has turned to blood. The red seeps into the land of Egypt and stains the hands of the Egyptians with the undeniable red, human iron. Yet Pharaoh hardened his heart once more.")
-print ("The Egyptian pagan magicians have turned the blood back to water, but their eyes betray their disraught. Shall you show them 'mercy' or 'continue'?")
-answer = input("Enter your decision:")
+  print ("The Nile has turned to blood. The red seeps into the land of Egypt and stains the hands of the Egyptians with the undeniable red, human iron. Yet Pharaoh hardened his heart once more.")
+  print ("The Egyptian pagan magicians have turned the blood back to water, but their eyes betray their disraught. Shall you show them 'mercy' or 'continue'?")
+  answer = input("Enter your decision:")
 
-if answer == "continue":
-  print ("You have proceeded with God's will. Pharaoh and his people shall suffer for the crimes of blood dripping from their hands. The second plague shall begin.") 
+  if answer == "continue":
+    print ("You have proceeded with God's will. Pharaoh and his people shall suffer for the crimes of blood dripping from their hands. The second plague shall begin.") 
 
-elif answer == "mercy":
-  print ("There is no rest for the wicked. God is merciful and patient, but he is not complacent. No mercy is shown to the unmerciful; you have strayed from God's chosen path. You are undeserving of his will.")
-  print("Your story has ended. Would you like to restart?")
+  elif answer == "mercy":
+    print ("There is no rest for the wicked. God is merciful and patient, but he is not complacent. No mercy is shown to the unmerciful; you have strayed from God's chosen path. You are undeserving of his will.")
+    print("Your story has ended. Would you like to restart?")
     restart = input("(yes/no): ")
     if restart != "yes":
       break
+    else:
+      continue
 
-print ("You tell the Pharaoh, 'the God of the Hebrews demands you to let his people go.' Pharaoh does not. The second of the plagues has begun. Frogs cover and stink the land of Egypt.")
-print ("And the people of Egypt have cried out, their lands drenched and the smell of the mass of frogs. Will your 'mercy' prevail or shall you let them 'suffer'?")
-answer = input("Enter your decision:")
+  print ("You tell the Pharaoh, 'the God of the Hebrews demands you to let his people go.' Pharaoh does not. The second of the plagues has begun. Frogs cover and stink the land of Egypt.")
+  print ("And the people of Egypt have cried out, their lands drenched and the smell of the mass of frogs. Will your 'mercy' prevail or shall you let them 'suffer'?")
+  answer = input("Enter your decision:")
 
-if answer == "mercy":
-  print ("You have cried out for the Lord to show the people mercy. With his heart, God has granted a moment of mercy upon the lands of Egypt. Yet, Pharaoh hardened his heart.")
+  if answer == "mercy":
+    print ("You have cried out for the Lord to show the people mercy. With his heart, God has granted a moment of mercy upon the lands of Egypt. Yet, Pharaoh hardened his heart.")
 
-elif answer == "suffer":
-  print ("God's will shall be done with or without mercy. There is no mercy shown to the Egyptians; the next plague shall proceed.")
+  elif answer == "suffer":
+    print ("God's will shall be done with or without mercy. There is no mercy shown to the Egyptians; the next plague shall proceed.")
 
-print ("Pharaoh hath hardened his heart once more against the God of the Hebrews. So be it, for the remaining plagues shall begin. There is no mercy for Pharaoh, for Pharaoh desired no mercy for himself.")
-print ("Lice have overtaken Egypt. Pharaoh's people cry out and scratch until their heads bleed of the sins of their leader. Yet Pharaoh hath hardened his heart.")
-print ("Swarms of flies have flown into Egypt. Their crops are gone, their land corrupted. The Pharaoh has called you in.")
+  print ("Pharaoh hath hardened his heart once more against the God of the Hebrews. So be it, for the remaining plagues shall begin. There is no mercy for Pharaoh, for Pharaoh desired no mercy for himself.")
+  print ("Lice have overtaken Egypt. Pharaoh's people cry out and scratch until their heads bleed of the sins of their leader. Yet Pharaoh hath hardened his heart.")
+  print ("Swarms of flies have flown into Egypt. Their crops are gone, their land corrupted. The Pharaoh has called you in.")
 
-print ("Pharaoh says that 'Your people shall be let go, but pray, pray that your God removes the corruption of my land.' Do you 'believe' him or 'proceed' with the plagues?")
-answer = input("Enter your decision:")
+  print ("Pharaoh says that 'Your people shall be let go, but pray, pray that your God removes the corruption of my land.' Do you 'believe' him or 'proceed' with the plagues?")
+  answer = input("Enter your decision:")
 
-if answer == "believe":
-  print ("There is no mercy in the eyes of Pharaoh. Only the heathen expression of the man with a hardened heart. He did not let God's people go. Proceed with the next plague.")
+  if answer == "believe":
+    print ("There is no mercy in the eyes of Pharaoh. Only the heathen expression of the man with a hardened heart. He did not let God's people go. Proceed with the next plague.")
 
-elif answer == "proceed":
-  print ("And Pharaoh's people cry out as the God of the Hebrews descends disease upon their livestock. There is no rest, no food for the people of the corrupt. Their babies and women cry out.")
+  elif answer == "proceed":
+    print ("And Pharaoh's people cry out as the God of the Hebrews descends disease upon their livestock. There is no rest, no food for the people of the corrupt. Their babies and women cry out.")
 
-print ("Such is life for the wicked.")
-print ("God hath spoken to you, 'Spread these fine ashes and the people of Egypt will have their skin boil.', and so be it. You spread the ashes into the wind. The skins of the Egyptians break out and boils eat them up alive with agony.")
+  print ("Such is life for the wicked.")
+  print ("God hath spoken to you, 'Spread these fine ashes and the people of Egypt will have their skin boil.', and so be it. You spread the ashes into the wind. The skins of the Egyptians break out and boils eat them up alive with agony.")
 
-print ("God has assigned you with another task: Announce the plagues that shall befall upon the lands of Egypt.")
+  print ("God has assigned you with another task: Announce the plagues that shall befall upon the lands of Egypt.")
 
-plagues = ["Hail", "Locust", "Darkness"]
-for x in plagues:
-    print(x)
+  plagues = ["Hail", "Locust", "Darkness"]
+  for x in plagues:
+      print(x)
 
-print ("And the people of Pharaoh cry out. Screams of terror and pain befall the destroyed land of Egypt. 'Mercy! Mercy!' crieth out the Egyptians.")
-print ("Our children and women are dying, disease blisters upon our people, and weep!")
-print ("What shall you do? Do the children and women deserve 'mercy' or shall you 'proceed' with the damnation that shall soon befall upon them?")
-answer = input("Enter your decision:")
+  print ("And the people of Pharaoh cry out. Screams of terror and pain befall the destroyed land of Egypt. 'Mercy! Mercy!' crieth out the Egyptians.")
+  print ("Our children and women are dying, disease blisters upon our people, and weep!")
+  print ("What shall you do? Do the children and women deserve 'mercy' or shall you 'proceed' with the damnation that shall soon befall upon them?")
+  answer = input("Enter your decision:")
 
-if answer == "proceed":
-  print ("God's will shall be done. The sins of the old shall be the sins of them all, for they disobey the Almighty God and abuse his people. Rain upon them the final plague...")
+  if answer == "proceed":
+    print ("God's will shall be done. The sins of the old shall be the sins of them all, for they disobey the Almighty God and abuse his people. Rain upon them the final plague...")
 
-elif answer == "mercy":
-  print ("Why have mercy for those who abuse God's people and defy his orders. Mercy is not for the wicked. (Not in the Old Testament.) You have left, straying from God's will. His will shall be done, but you are not.")
-  restart = input("(yes/no): ")
-    if restart != "yes":
-      break
-
-print ("Pharaoh has hardened his heart; in turn, God has hardened Pharaoh's heart. Your responsibility is to tell the people of God to mark their doors with a pure lamb's blood.")
-print ("The angel of death bellowed upon the land of Egypt.")
-print ("All the firstborns of the Egyptians have their lives taken by the Angel of Death sent by God.")
-
-deaths = ["Cry of the Babies", "Wail of the Parents", "Weep people of Egypt, for the God of the Hebrews' will has been executed."]
-for x in deaths:
-    print(x)
-
-print ("Leave! Leave and take thy people, and thy cattle, and thy resources! Leave and let the people of thy God worship Him!' exclaimed the Pharaoh to you. Devastation pours from his very being.")
-print ("Rejoice, people of God! For God has exacted his will, his mercy and grace abounds all! He shall lead us to the wilderness. Blessed be the name of the Lord!")
-print ("Peace doesn't stay long for Pharaoh hardens his heart for the last time and chases you and God's people down.")
-print ("The people of God cry out, 'Have you brought us out here to be killed in the wilderness! Why have you done this to us!?' Scrutiny and complaints surround you, and indirectly, they insult God.")
-print ("How do you deal with them? Shall you let them 'go' or 'persevere'?")
-answer = input("Enter your decision:")
-
-if answer == 'persevere':
-  print ("Blessed be the patient. Yes, the people complain. Yes, it is insulting. But, God is patient, learn to rely upon him even in the face of judgment. Soon, he shall bring you out of this...")
-
-elif answer == 'go':
-  print ("Are you above the Lord? He has taken this scrutiny, what makes you think you are better? For all sins are the same in the eyes of the Lord. Lay down your staff, undeserving leader.")
+  elif answer == "mercy":
+    print ("Why have mercy for those who abuse God's people and defy his orders. Mercy is not for the wicked. (Not in the Old Testament.) You have left, straying from God's will. His will shall be done, but you are not.")
     restart = input("(yes/no): ")
     if restart != "yes":
       break
+    else:
+      continue
+
+  print ("Pharaoh has hardened his heart; in turn, God has hardened Pharaoh's heart. Your responsibility is to tell the people of God to mark their doors with a pure lamb's blood.")
+  print ("The angel of death bellowed upon the land of Egypt.")
+  print ("All the firstborns of the Egyptians have their lives taken by the Angel of Death sent by God.")
+
+  deaths = ["Cry of the Babies", "Wail of the Parents", "Weep people of Egypt, for the God of the Hebrews' will has been executed."]
+  for x in deaths:
+      print(x)
+
+  print ("Leave! Leave and take thy people, and thy cattle, and thy resources! Leave and let the people of thy God worship Him!' exclaimed the Pharaoh to you. Devastation pours from his very being.")
+  print ("Rejoice, people of God! For God has exacted his will, his mercy and grace abounds all! He shall lead us to the wilderness. Blessed be the name of the Lord!")
+  print ("Peace doesn't stay long for Pharaoh hardens his heart for the last time and chases you and God's people down.")
+  print ("The people of God cry out, 'Have you brought us out here to be killed in the wilderness! Why have you done this to us!?' Scrutiny and complaints surround you, and indirectly, they insult God.")
+  print ("How do you deal with them? Shall you let them 'go' or 'persevere'?")
+  answer = input("Enter your decision:")
+
+  if answer == 'persevere':
+    print ("Blessed be the patient. Yes, the people complain. Yes, it is insulting. But, God is patient, learn to rely upon him even in the face of judgment. Soon, he shall bring you out of this...")
+
+  elif answer == 'go':
+    print ("Are you above the Lord? He has taken this scrutiny, what makes you think you are better? For all sins are the same in the eyes of the Lord. Lay down your staff, undeserving leader.")
+    restart = input("(yes/no): ")
+    if restart != "yes":
+      break
+    else:
+      continue
       
-print ("Moses! Hold out your staff! 'Hold' out your staff.")
-answer = input("Do as God commands:")
+  print ("Moses! Hold out your staff! 'Hold' out your staff.")
+  answer = input("Do as God commands:")
 
-if answer == "hold":
-  print ("The seas part with God's power, and the people of God travel through the Red Sea. However, the Egyptians try to follow...")
+  if answer == "hold":
+    print ("The seas part with God's power, and the people of God travel through the Red Sea. However, the Egyptians try to follow...")
 
-print ("The seas rush back as the Egyptians try to follow, effectively wiping them out.")
-print ("Praise the Lord of the Hebrews! Praise Him for his blessings! Praise the Almighty God! Blessed be His name!")
+  print ("The seas rush back as the Egyptians try to follow, effectively wiping them out.")
+  print ("Praise the Lord of the Hebrews! Praise Him for his blessings! Praise the Almighty God! Blessed be His name!")
