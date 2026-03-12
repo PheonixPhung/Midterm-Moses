e142e9b7a33f23aad519a0c2ada6969d7936c99d
PheonixPhung <phung.huynhsaca@gmail.com>
2026-03-12 01:56:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ if answer == "continue":
 
 elif answer == "mercy":
   print ("There is no rest for the wicked. God is merciful and patient, but he is not complacent. No mercy is shown to the unmerciful; you have strayed from God's chosen path. You are undeserving of his will.")
+  print("Your story has ended. Would you like to restart?")
+    restart = input("(yes/no): ")
+    if restart != "yes":
+      break
 
 print ("You tell the Pharaoh, 'the God of the Hebrews demands you to let his people go.' Pharaoh does not. The second of the plagues has begun. Frogs cover and stink the land of Egypt.")
 print ("And the people of Egypt have cried out, their lands drenched and the smell of the mass of frogs. Will your 'mercy' prevail or shall you let them 'suffer'?")
@@ -58,6 +62,7 @@ elif answer == "suffer":
 print ("Pharaoh hath hardened his heart once more against the God of the Hebrews. So be it, for the remaining plagues shall begin. There is no mercy for Pharaoh, for Pharaoh desired no mercy for himself.")
 print ("Lice have overtaken Egypt. Pharaoh's people cry out and scratch until their heads bleed of the sins of their leader. Yet Pharaoh hath hardened his heart.")
 print ("Swarms of flies have flown into Egypt. Their crops are gone, their land corrupted. The Pharaoh has called you in.")
+
 print ("Pharaoh says that 'Your people shall be let go, but pray, pray that your God removes the corruption of my land.' Do you 'believe' him or 'proceed' with the plagues?")
 answer = input("Enter your decision:")
 
@@ -65,9 +70,62 @@ if answer == "believe":
   print ("There is no mercy in the eyes of Pharaoh. Only the heathen expression of the man with a hardened heart. He did not let God's people go. Proceed with the next plague.")
 
 elif answer == "proceed":
-  print ("And Pharaoh's people cry out as the God of the Hebrews descends disease upon their livestock. There is no rest, no food for the people of the corrupt. Yet their babies and women cry out.")
+  print ("And Pharaoh's people cry out as the God of the Hebrews descends disease upon their livestock. There is no rest, no food for the people of the corrupt. Their babies and women cry out.")
 
 print ("Such is life for the wicked.")
 print ("God hath spoken to you, 'Spread these fine ashes and the people of Egypt will have their skin boil.', and so be it. You spread the ashes into the wind. The skins of the Egyptians break out and boils eat them up alive with agony.")
 
+print ("God has assigned you with another task: Announce the plagues that shall befall upon the lands of Egypt.")
 
+plagues = ["Hail", "Locust", "Darkness"]
+for x in plagues:
+    print(x)
+
+print ("And the people of Pharaoh cry out. Screams of terror and pain befall the destroyed land of Egypt. 'Mercy! Mercy!' crieth out the Egyptians.")
+print ("Our children and women are dying, disease blisters upon our people, and weep!")
+print ("What shall you do? Do the children and women deserve 'mercy' or shall you 'proceed' with the damnation that shall soon befall upon them?")
+answer = input("Enter your decision:")
+
+if answer == "proceed":
+  print ("God's will shall be done. The sins of the old shall be the sins of them all, for they disobey the Almighty God and abuse his people. Rain upon them the final plague...")
+
+elif answer == "mercy":
+  print ("Why have mercy for those who abuse God's people and defy his orders. Mercy is not for the wicked. (Not in the Old Testament.) You have left, straying from God's will. His will shall be done, but you are not.")
+  print("Your story has ended. Would you like to restart?")
+    restart = input("(yes/no): ")
+    if restart != "yes":
+      break
+
+print ("Pharaoh has hardened his heart; in turn, God has hardened Pharaoh's heart. Your responsibility is to tell the people of God to mark their doors with a pure lamb's blood.")
+print ("The angel of death bellowed upon the land of Egypt.")
+print ("All the firstborns of the Egyptians have their lives taken by the Angel of Death sent by God.")
+
+deaths = ["Cry of the Babies", "Wail of the Parents", "Weep people of Egypt, for the God of the Hebrews' will has been executed."]
+for x in deaths:
+    print(x)
+
+print ("Leave! Leave and take thy people, and thy cattle, and thy resources! Leave and let the people of thy God worship Him!' exclaimed the Pharaoh to you. Devastation pours from his very being.")
+print ("Rejoice, people of God! For God has exacted his will, his mercy and grace abounds all! He shall lead us to the wilderness. Blessed be the name of the Lord!")
+print ("Peace doesn't stay long for Pharaoh hardens his heart for the last time and chases you and God's people down.")
+print ("The people of God cry out, 'Have you brought us out here to be killed in the wilderness! Why have you done this to us!?' Scrutiny and complaints surround you, and indirectly, they insult God.")
+print ("How do you deal with them? Shall you let them 'go' or 'persevere'?")
+answer = input("Enter your decision:")
+
+if answer == 'persevere':
+  print ("Blessed be the patient. Yes, the people complain. Yes, it is insulting. But, God is patient, learn to rely upon him even in the face of judgment. Soon, he shall bring you out of this...")
+
+elif answer == 'go':
+  print ("Are you above the Lord? He has taken this scrutiny, what makes you think you are better? For all sins are the same in the eyes of the Lord. Lay down your staff, undeserving leader.")
+  print("Your story has ended. Would you like to restart?")
+    restart = input("(yes/no): ")
+    if restart != "yes":
+      break
+
+print ("Moses! Hold out your staff! 'Hold' out your staff.")
+answer = input("Do as God commands:")
+
+if answer == "hold":
+  print ("The seas part with God's power and the people of God travel through the Red Sea. However, the Egyptians try to follow...")
+
+print ("The seas rush back as the Egyptians try to follow, effectively wiping them out.")
+print ("Praise the Lord of the Hebrews! Praise Him for his blessings! Praise the Almighty God! Blessed be His name!")
